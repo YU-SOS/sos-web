00f853e5dd1fa67ecb107b86621de50bcaddd227
hy2 <126475386+hysw-2@users.noreply.github.com>
2024-09-22 17:33:15 +0900

    Revert "Feature: 로그인, 회원가입 기능 추가 (#4)"
    This reverts commit 0c4108c67f1c9847f1f329aa5231b1a9e5ef27e8.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Login from './pages/Login';
-import SignupAdmin from './pages/SignupAdmin';
-import SignupAmbulance from './pages/SignupAmbulance';
-import SignupHospital from './pages/SignupHospital';
+import Homepage from './pages/homepage';
+import LoginAmbulanceAndHospital from './pages/loginAmbulanceAndHospital';
+import LoginGeneralUser from './pages/loginGeneralUser';
+import SignupAmbulance from './pages/signupAmbulance';
+import SignupHospital from './pages/signupHospital';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-
-                <Route path="/login" element={<Login />} />
-
-                <Route path="/signup/admin" element={<SignupAdmin />} />
-                <Route path="/signup/ambulance" element={<SignupAmbulance />} />
-                <Route path="/signup/hospital" element={<SignupHospital />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login/general" element={<LoginGeneralUser />} />
+        <Route path="/login" element={<LoginAmbulanceAndHospital />} />
+        <Route path="/signup/ambulance" element={<SignupAmbulance />} />
+        <Route path="/signup/hospital" element={<SignupHospital />} />
+      </Routes>
+    </Router>
+  );
 };
-//
+
 export default App;
